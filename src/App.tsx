4b97929b1afd3325bd989d19bb85cfc9db868d52
MohammadWaseem6 <mohammadwaseem001100@gmail.com>
2025-09-07 22:06:40 +0300

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,45 @@
-import React from 'react'
-import Navbar from './Pages/Navbar'
-import Home from './Pages/Home'
-import About from './Pages/About'
-import Projects from './Pages/Projects'
-import Footer from './Pages/Footer'
+import React, { useState, useEffect } from 'react';
+import Navbar from './Pages/Navbar';
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Projects from './Pages/Projects';
+import Footer from './Pages/Footer';
 
 const App: React.FC = () => {
-  return (
-    <div>
-      <Navbar />
-      <Home />
-      <About />
-      <Projects/>
-      <Footer/>
-     
-    </div>
-  )
-}
+  const [activeSection, setActiveSection] = useState('home');
 
-export default App
+  useEffect(() => {
+    const handleScroll = () => {
+      const sections = ['home', 'about', 'projects'];
+      const scrollPosition = window.scrollY + 100;
+
+      for (const section of sections) {
+        const element = document.getElementById(section);
+        if (element) {
+          const offsetTop = element.offsetTop;
+          const offsetHeight = element.offsetHeight;
+          
+          if (scrollPosition >= offsetTop && scrollPosition < offsetTop + offsetHeight) {
+            setActiveSection(section);
+            break;
+          }
+        }
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  return (
+    <div className="min-h-screen bg-gray-900 text-white">
+      <Navbar activeSection={activeSection} setActiveSection={setActiveSection} />
+      <Home id="home" />
+      <About id="about" />
+      <Projects id="projects" />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
